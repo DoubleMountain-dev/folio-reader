39d6a79e85ebd29b5df2ae5f0a961066fa9570ad
Green <mikstron1@gmail.com>
2026-05-12 01:44:22 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,21 @@ import 'package:provider/provider.dart';
 import 'providers/library_provider.dart';
 import 'screens/main_screen.dart';
 import 'services/sound_service.dart';
+import 'services/tts_service.dart';
+import 'services/font_loader.dart';
 import 'utils/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // Initialise sound service (loads saved preferences)
   await SoundService.instance.init();
+  await TtsService.instance.init();
+  await CustomFontLoader.instance.restore();
   runApp(const FolioApp());
 }
 
 class FolioApp extends StatelessWidget {
   const FolioApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
+  @override Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => LibraryProvider(),
       child: MaterialApp(
